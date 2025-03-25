dea4af813e7ba92c8c8e0d8a3d49cc4956044721
Pham Van Loc <phamloc842002@gmail.com>
2025-03-25 22:18:24 +0700

    To Create Target Tracking App

---
+++ lib/core/style/colors.dart
@@ -0,0 +1,17 @@
+
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+const Color colorPrinciple = Color(0xFF3C3793);
+const Color colorStartButton = Color(0xFF1CA4B4);
+const Color colorWhite = Colors.white;
+const Color colorBlack = Colors.black;
+const Color colorGrey = Color(0xFF8B8E8D);
+const Color colorDarkYellow = Color(0xFFFCAC2F);
+const Color colorBlue = Color(0xFF165DD4);
+const Color colorDarkPink = Color(0xFFC84B79);
+const Color colorDarkRed = Color(0xFFD73030);
+const Color colorRed = Color(0xFFFF0000);
+const Color colorBlueMint = Color(0xFF03BBD7);
+const Color colorLightGreen = Color(0xFF9BE46C);
+const Color colorGreen = Color(0xFF02B936);
